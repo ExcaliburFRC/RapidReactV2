6d4d2fe4f09dc75536cdb93c75015c1cc4f15da7
Excalibur FRC | 6738 <excaliburfrc@gmail.com>
2022-11-06 20:17:58 +0200

    init

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,98 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final class IntakeConstants {
+        public static final int INTAKE_MOTOR_ID = 40; // PDP 4
+        public static final int UPPER_MOTOR_ID = 41; // PDP 11
+        public static final int UPPER_PING = 7;
+        public static final int UPPER_ECHO = 6;
+        public static final int COLOR_LIMIT = 55;
+        public static final int SONIC_LIMIT = 150;
+        public static final int MAX_BALLS = 2;
+        public static final int FWD_CHANNEL = 3;
+        public static final int REV_CHANNEL = 4;
+        public static final double HATCH_KP = 0;
+        public static final double OPEN_POS = 90;
+        public static final double CLOSE_POS = 0;
+      }
+    
+      public static class ClimberConstants {
+        public static final int FORWARD_CHANNEL = 1;
+        public static final int REVERSE_CHANNEL = 7;
+        public static final int LEFT_MOTOR_ID = 30;
+        public static final int RIGHT_MOTOR_ID = 31;
+    
+        public static final float FORWARD_SOFT_LIMIT = 64.7f;
+        public static final float REVERSE_SOFT_LIMIT = 2f;
+    
+        public static final Value ANGLED = Value.kReverse;
+        public static final Value STRAIGHT = Value.kForward;
+      }
+    
+      public static class ShooterConstants {
+        public static final int LEADER_ID = 20;
+        public static final int FOLLOWER_ID = 21;
+        public static final int ENCODER_A = 0;
+        public static final int ENCODER_B = 1;
+    
+        public static final double kS = 0.10898;
+        public static final double kV = 0.065;
+        //    public static final double kA = 0.031584; // we want 0 acceleration
+        public static final double kP = 0.3;
+    
+        public static final double RATIO = 42.0 / 18.0;
+        public static final double CPR = 1024;
+        public static final double ROTATIONS_PER_PULSE = RATIO / CPR;
+    
+        // 121.45182291666667;
+        public static final double FENDER_SHOT_RPM = 68;
+        public static final double TOLERANCE = 4;
+      }
+    
+      public static class DrivetrainConstants {
+        public static final int LEFT_LEADER_ID = 10;
+        public static final int RIGHT_LEADER_ID = 11;
+        public static final int LEFT_FOLLOWER_ID = 12;
+        public static final int RIGHT_FOLLOWER_ID = 13;
+    
+        public static final double GEARING = 1.0 / 10.71;
+    
+        public static final double MOTOR_ROTATION_TO_METERS =
+            GEARING * Math.PI * Units.inchesToMeters(6);
+    
+        public static final Translation2d HUB_POS = new Translation2d(8.247, 4.092);
+    
+        public static final double kS = 0.13086;
+        public static final double kV = 2.8806;
+        public static final double kA = 0.39188;
+        public static final double kP = 0.045064;
+        public static final DifferentialDriveKinematics driveKinematics =
+            new DifferentialDriveKinematics(0.70);
+        public static final double kP_ang = 0.055;
+    
+        public static final Pose2d LOWER_FENDER = new Pose2d(7.797, 2.821, Rotation2d.fromDegrees(75));
+      }
+    
+      public static class LedsConstants {
+        public static final int LEDS_PORT = 0;
+      }
+}
---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import java.util.function.BooleanSupplier;
+
+public class Climber extends SubsystemBase {
+  private final DoubleSolenoid piston =
+        new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.ClimberConstants.FORWARD_CHANNEL,
+              Constants.ClimberConstants.REVERSE_CHANNEL);
+  private final CANSparkMax rightMotor =
+        new CANSparkMax(Constants.ClimberConstants.RIGHT_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax leftMotor =
+        new CANSparkMax(Constants.ClimberConstants.LEFT_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+  public Climber() {
+    leftMotor.setInverted(true);
+    rightMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    leftMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+  }
+
+  public Command emptyCommand(){
+    return new RunCommand(()-> {}, this);
+  }
+  public Command openPistonCommand() {
+    return new InstantCommand(()->piston.set(DoubleSolenoid.Value.kForward));
+  }
+  public Command closePistonCommand() {
+    return new InstantCommand(()->piston.set(DoubleSolenoid.Value.kReverse));
+  }
+  public Command manualCommand(
+        BooleanSupplier leftUp, BooleanSupplier leftDown,
+        BooleanSupplier rightUp, BooleanSupplier rightDown,
+        BooleanSupplier openPiston, BooleanSupplier closePiston){
+    return new RunCommand(
+          ()-> {
+            if (leftUp.getAsBoolean()) leftMotor.set(0.5);
+            else if (leftDown.getAsBoolean()) leftMotor.set(-0.5);
+            else leftMotor.set(0);
+            if (rightUp.getAsBoolean()) rightMotor.set(0.5);
+            else if (rightDown.getAsBoolean()) rightMotor.set(-0.5);
+            else rightMotor.set(0);
+
+            if (openPiston.getAsBoolean()) piston.set(DoubleSolenoid.Value.kReverse);
+            if (closePiston.getAsBoolean()) piston.set(DoubleSolenoid.Value.kForward);
+          }, this);
+  }
+}
