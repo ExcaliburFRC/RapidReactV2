6a85afc8675f812f37a204688d792c93ec333929
Excalibur FRC | 6738 <excaliburfrc@gmail.com>
2022-11-28 16:04:38 +0200

    continued to work on the intake classes inner functions, continues work on the Superstructure intakeBallsCommand.

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -2,33 +2,28 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Constants;
+import lib.RepeatingCommand;
 
 public class Superstructure {
   private final Intakee intake = new Intakee();
   private final Shooter shooter = new Shooter();
 
   public Command intakeBallsCommand() {
-    return new FunctionalCommand(
-          () -> intake.openClosePiston(true),
-          () -> {
-            intake.setFrontMotorSpeed(0.3);
-          },
-          (__) -> {
-          },
-          () -> intake.getColorDis() < Constants.IntakeConstants.COLOR_LIMIT)
+    return new InstantCommand(
+          () -> intake.openClosePiston(true))
           .andThen(
+                new RepeatingCommand(
                 new ConditionalCommand(
                       // deciding by whether the ball is our color
                       new ConditionalCommand(
                             // whether the ultrasonic detects a ball
                             intake.closeIntake(),
                             intake.pullToUltrasonic(),
-                            // TODO portal to A
-
                             intake::isUltraDist
                       ),
                       new ConditionalCommand(
                             // whether we want to eject from intake or from shooter
+                            intake.ejectFromColor()
                             ,
                             ,
                             intake::isUltraDist
